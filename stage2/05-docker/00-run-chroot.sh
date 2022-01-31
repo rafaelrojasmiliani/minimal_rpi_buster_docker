ef81b91fe4d2fd1e20037e3dcb0c6e3ca6c77677
Rojas Rafael <rafael.rojas@unibz.it>
2022-01-31 17:11:46 +0100

    add ssl cert fix

--- stage2/05-docker/00-run-chroot.sh
+++ stage2/05-docker/00-run-chroot.sh
@@ -3,6 +3,7 @@
 
 main(){
     cd / && \
+    export SSL_CERT_FILE=/usr/lib/ssl/certs/ca-certificates.crt
     curl -fsSL https://get.docker.com -o get-docker.sh && \
     sh get-docker.sh
     sudo usermod -aG docker ${FIRST_USER_NAME}
